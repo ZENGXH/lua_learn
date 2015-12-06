885a446f78208d1b47a56ad669ed99e79b288344
Xiaohui Zeng <xzengaf@ust.hk>
2015-12-07 00:27:37 +0100

    12/7 clean repo

--- project1/leastSquares.m
+++
@@ -1,3 +0,0 @@
-function beta = leastSquares(y, tX)
-% using normal equation
-beta = pinv(tX' * tX) * tX' * y;
--- project1/ridgeRegression.m
+++
@@ -1,5 +0,0 @@
-function beta = ridgeRegression(y, tX, lambda)
-lambdaI = lambda * eye(length(tX' * tX));
-lambdaI(1,1) = 0;
-beta = pinv(tX' * tX + lambdaI) * tX' * y;
-end
